45b02deedc5f5fc778b6f8fbe818039bf0738249
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-05-09 09:18:04 +0530

    ex5 hw4 done

--- hw4/05-form.js
+++ hw4/05-form.js
@@ -1,7 +1,42 @@
-let form = document.querySelector('form');
+let form = document.querySelector("form");
+const defaultValue = "no submission";
 
-form.addEventListener('submit', handleSubmit);
+form.addEventListener("submit", handleSubmit);
 
 function handleSubmit(event) {
   // Add your code here
+  const username = document.getElementById("username");
+  const usernameValue =
+    username.value.length > 0 ? username.value : defaultValue;
+
+  const email = document.getElementById("email");
+  const emailValue = email.value.length > 0 ? email.value : defaultValue;
+
+  const signup = document.getElementsByName("signup");
+  let selected = defaultValue;
+  for (const radioButton of signup) {
+    if (radioButton.checked) {
+      selected = radioButton.value;
+      break;
+    }
+  }
+
+  const date = document.getElementById("date");
+  const dateValue = date.value.length > 0 ? date.value : defaultValue;
+  if (
+    usernameValue === defaultValue &&
+    emailValue === defaultValue &&
+    selected === defaultValue &&
+    dateValue === defaultValue
+  ) {
+    console.warn("You must enter some data to submit this form");
+  } else {
+    console.log("================ Form Submission ==================");
+    console.log("Username: " + usernameValue);
+    console.log("Email: " + emailValue);
+    console.log("Newsletter: " + selected);
+    console.log("Date: " + dateValue);
+  }
+
+  event.preventDefault();
 }
