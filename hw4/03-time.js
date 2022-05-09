f8f6d14accbaad0094b3c3d1ea83a6d5eeb8964e
Shreyoshi Mahato <shreyoshimahato@gmail.com>
2022-05-09 06:23:20 +0530

    time js commit hw4

--- hw4/03-time.js
+++ hw4/03-time.js
@@ -1,5 +1,14 @@
 const calculateTime = (date1, date2) => {
   // Given two dates, calculate and return the amount of time elapsed in years and months
+  let start = new Date(date1);
+  let end = new Date(date2);
+  if (isNaN(start) || isNaN(end)) {
+    return "Error: Invalid input provided.";
+  }
+  let year = start.getFullYear() - end.getFullYear();
+  let month = start.getMonth() - end.getMonth();
+  if (year === 0) return month + " months";
+  return year + " years, " + month + " months";
 };
 
 // Date() formats:
@@ -8,13 +17,13 @@ const calculateTime = (date1, date2) => {
 // new Date(2020, 3, 16)
 // new Date(628021800000)
 
-console.log(calculateTime(1635176171332, 'May 1, 1995'));
+console.log(calculateTime(1635176171332, "May 1, 1995"));
 // Time elapsed: 26 years, 5 months
-console.log(calculateTime(1635176171332, '1975-8-11'));
+console.log(calculateTime(1635176171332, "1975-8-11"));
 // Time elapsed: 46 years, 2 months
 console.log(calculateTime(1635176171332, [2021, 5, 23]));
 // Time elapsed: 5 months
 console.log(calculateTime(1635176171332, 1031814000000));
 // Time elapsed: 19 years, 1 month
-console.log(calculateTime(1635176171332, 'birthdate'));
+console.log(calculateTime(1635176171332, "birthdate"));
 // Error: Invalid input provided.
